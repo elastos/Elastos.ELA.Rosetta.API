6ec5419b180c0e2328866610a2dc1ffcf1c7be14
xiaoming <1406629722@qq.com>
2022-01-17 12:28:27 +0800

    feat(test): seperate client test& add mempool test

---
+++ client/mempool_test.go
@@ -0,0 +1,76 @@
+package client
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/coinbase/rosetta-sdk-go/asserter"
+	"github.com/coinbase/rosetta-sdk-go/types"
+	"github.com/elastos/Elastos.ELA.Rosetta.API/common/base"
+)
+
+func init() {
+	t := new(testing.T)
+	Test_network(t)
+}
+
+func Test_RunTestMempoolInloop(t *testing.T) {
+	for {
+		Test_Mempool(t)
+		time.Sleep(30 * time.Second)
+	}
+}
+
+func Test_Mempool(t *testing.T) {
+	client := create_test_client()
+	ctx := context()
+	if primaryNetwork == nil || networkStatus == nil || networkOptions == nil {
+		log.Fatal("primaryNetwork or networkStatus or networkOptions has not been initialized")
+	}
+	request := &types.NetworkRequest{
+		NetworkIdentifier: primaryNetwork,
+		Metadata:          nil,
+	}
+	mempool, rosettaErr, err := client.MempoolAPI.Mempool(ctx, request)
+
+	if rosettaErr != nil {
+		log.Printf("Rosetta Error: %+v\n", rosettaErr)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Current Mempool transaction hashes : %s\n", types.PrettyPrintStruct(mempool))
+	// The asserter automatically rejects incorrectly formatted
+	// requests.
+	asserter, err := asserter.NewServer(
+		[]string{base.MainnetNextworkType},
+		false,
+		[]*types.NetworkIdentifier{primaryNetwork},
+		nil,
+		false,
+		"",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, txIdent := range mempool.TransactionIdentifiers {
+		memTxReq := &types.MempoolTransactionRequest{
+			NetworkIdentifier:     primaryNetwork,
+			TransactionIdentifier: txIdent,
+		}
+		err := asserter.MempoolTransactionRequest(memTxReq)
+		if err != nil {
+			log.Fatalf("Assertion Error: %s\n", err.Error())
+		}
+		txResp, rosettaErr, err := client.MempoolAPI.MempoolTransaction(ctx, memTxReq)
+		if rosettaErr != nil {
+			log.Printf("Rosetta Error: %+v\n", rosettaErr)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("Mempool transaction : %s\n", types.PrettyPrintStruct(txResp))
+	}
+}
---
+++ client/network_test.go
@@ -0,0 +1,107 @@
+// Copyright 2020 Coinbase, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package client
+
+import (
+	"log"
+	"testing"
+
+	"github.com/coinbase/rosetta-sdk-go/asserter"
+	"github.com/coinbase/rosetta-sdk-go/types"
+)
+
+var (
+	primaryNetwork *types.NetworkIdentifier
+	networkStatus  *types.NetworkStatusResponse
+	networkOptions *types.NetworkOptionsResponse
+)
+
+func Test_network(t *testing.T) {
+	ctx := context()
+	client := create_test_client()
+	var rosettaErr *types.Error
+	var err error
+	// Step 2: Get all available networks
+	var networkList *types.NetworkListResponse
+	networkList, rosettaErr, err = client.NetworkAPI.NetworkList(
+		ctx,
+		&types.MetadataRequest{},
+	)
+	if rosettaErr != nil {
+		log.Printf("Rosetta Error: %+v\n", rosettaErr)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := asserter.NetworkListResponse(networkList); err != nil {
+		log.Fatal(err)
+	}
+
+	if len(networkList.NetworkIdentifiers) == 0 {
+		log.Fatal("no available networks")
+	}
+
+	primaryNetwork = networkList.NetworkIdentifiers[0]
+
+	// Step 3: Print the primary network
+	log.Printf("Primary Network: %s\n", types.PrettyPrintStruct(primaryNetwork))
+
+	// Step 4: Fetch the network status
+	networkStatus, rosettaErr, err = client.NetworkAPI.NetworkStatus(
+		ctx,
+		&types.NetworkRequest{
+			NetworkIdentifier: primaryNetwork,
+		},
+	)
+	if rosettaErr != nil {
+		log.Printf("Rosetta Error: %+v\n", rosettaErr)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Step 5: Print the response
+	log.Printf("Network Status: %s\n", types.PrettyPrintStruct(networkStatus))
+
+	// Step 6: Assert the response is valid
+	err = asserter.NetworkStatusResponse(networkStatus)
+	if err != nil {
+		log.Fatalf("Assertion Error: %s\n", err.Error())
+	}
+
+	// Step 7: Fetch the network options
+	networkOptions, rosettaErr, err = client.NetworkAPI.NetworkOptions(
+		ctx,
+		&types.NetworkRequest{
+			NetworkIdentifier: primaryNetwork,
+		},
+	)
+	if rosettaErr != nil {
+		log.Printf("Rosetta Error: %+v\n", rosettaErr)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Step 8: Print the response
+	log.Printf("Network Options: %s\n", types.PrettyPrintStruct(networkOptions))
+
+	// Step 9: Assert the response is valid
+	err = asserter.NetworkOptionsResponse(networkOptions)
+	if err != nil {
+		log.Fatalf("Assertion Error: %s\n", err.Error())
+	}
+}
